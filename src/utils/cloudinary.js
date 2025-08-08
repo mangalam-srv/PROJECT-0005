e1e7102301e9978577fda385e76cc5b303bd6d74
mangalam-srv <mangalamstp2005@gmail.com>
2025-08-08 16:45:29 +0530

    new

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import {v2 as cloudinary}from "cloudinary";
+import { log } from "console";
+import fs from "fs"; 
+
+
+cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_NAME, 
+        api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+
+
+const uploadoncloudinary = async (localfilepath)=>{
+    try {
+
+        //if no file is provided
+        if(!localfilepath)return null;
+
+        //if file is provided
+        const response = await cloudinary.uploader.upload(localfilepath,{resource_type:"auto"})
+
+        console.log("file is successfully uploaded",cloudinary.url);
+        
+        return response;
+        
+    } catch (error) {
+
+        fs.unlinkSync(localfilepath);
+        return null;    
+        
+    }
+}
+
+export{uploadoncloudinary}; 
